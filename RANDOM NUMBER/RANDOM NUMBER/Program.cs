c27bbfc032400d5ed7fcccfab98725a0495b6066
SHEFDAN <110844468+SHEFDAN@users.noreply.github.com>
2022-08-19 08:38:45 -0700

    Add files via upload

---
+++ RANDOM NUMBER/RANDOM NUMBER/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RANDOM_NUMBER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                //Console.writeline(random.Next());
+                SimpleMethod(random);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void SimpleMethod(Random random)
+        {
+            Console.WriteLine(random.Next());
+
+        }
+    }
+}
